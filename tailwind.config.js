d919a3c2045f761263b10b70a2148719254c4a8f
robimalco <robimalco@gmail.com>
2021-09-25 23:39:32 +0200

    #5 Design New Trade Section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,33 @@
 module.exports = {
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       fontFamily: {
-        'montserrat': ['Montserrat']
+        "montserrat": ["Montserrat"]
       },
       colors: {
-        header: '#00BED5',
+        // fonts
+        tradebigtext: "#104248",
+        tradesmalltext: "#1A1A1A",
+        // backgrounds
+        header: "#00BED5",
         tradehero: "#3400FA",
         tradetablehead: "#D2F8FD",
-        tradepositive: '#00BED5',
-        tradenegative: '#FA6060'
+        tradepositive: "#00BED5",
+        tradenegative: "#FA6060",
+        tradenew: "#D2F8FD",
+        subtradenew: "#AEEBF3",
+        tradenewbuttonconfirm: "#3400FA", 
+        tradenewbuttoncancel: "#B8E7EF"
       }
     },
   },
   variants: {
     extend: {
-      backgroundColor: ['even'],
-      fontSize: ['hover'],
-      fontWeight: ['hover'],
+      backgroundColor: ["even"],
+      fontSize: ["hover"],
+      fontWeight: ["hover"],
     },
   },
   plugins: [],
