5430b460f35b23d1fd7dd827ff96c1cb390dfcc7
robimalco <robimalco@gmail.com>
2021-09-30 06:54:04 +0200

    complete new password

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -13,6 +13,12 @@
         </div>
         <div
           class="px-2 py-2 pr-12 mr-1 rounded-md text-xol"
+          :class="settingsSection=='password' ? 'bg-tradehero text-white font-semibold' : 'bg-white'"
+          @click="changeSettingsSection('password')">
+          Password
+        </div>
+        <div
+          class="px-2 py-2 pr-12 mr-1 rounded-md text-xol"
           :class="settingsSection=='privacy' ? 'bg-tradehero text-white font-semibold' : 'bg-white'"
           @click="changeSettingsSection('privacy')">
           Privacy
@@ -22,12 +28,6 @@
           :class="settingsSection=='plan' ? 'bg-tradehero text-white font-semibold' : 'bg-white'"
           @click="changeSettingsSection('plan')">
           Plan
-        </div>
-        <div
-          class="px-2 py-2 pr-12 mr-1 rounded-md text-xol"
-          :class="settingsSection=='password' ? 'bg-tradehero text-white font-semibold' : 'bg-white'"
-          @click="changeSettingsSection('password')">
-          Password
         </div>
       </div>
       <div class="col-span-4">
@@ -49,7 +49,7 @@
             </label>
           </div>
           <div class="p-2">
-          <label class="text-xs text-subtradelabel">Email</label>
+            <label class="text-xs text-subtradelabel">Email</label>
             <input
               type="email"
               placeholder="Email"
@@ -93,14 +93,57 @@
             </button>
           </div>
         </section> 
+        <section v-if="settingsSection=='password'">
+          <div class="p-2">
+            <label class="text-xs text-subtradelabel">Old Password</label>
+            <input
+              type="password"
+              placeholder="Old Password"
+              class="w-full p-2 text-gray-800 border border-gray-200 border-gray-500"
+              v-model="userPassword.OldPassword">
+          </div>
+          <div class="p-2">
+            <label class="text-xs text-subtradelabel">New Password</label>
+            <input
+              type="password"
+              placeholder="New Password"
+              class="w-full p-2 text-gray-800 border border-gray-200 border-gray-500"
+              v-model="userPassword.NewPassword">
+          </div>
+          <div class="p-2">
+            <label class="text-xs text-subtradelabel">Repeat New Password</label>
+            <input
+              type="password"
+              placeholder="Repeat New Password"
+              class="w-full p-2 text-gray-800 border border-gray-200 border-gray-500"
+              v-model="userPassword.RepeatNewPassword">
+          </div>
+          <div class="flex justify-start">
+            <button
+              class="px-4 py-2 m-6 font-bold text-white rounded bg-header-light hover:bg-header-dark"
+              type="submit"
+              @click="saveUserPassword()">
+              Save
+            </button>
+            <button
+              class="px-4 py-2 m-6 font-bold text-white bg-green-500 rounded"
+              id="userPasswordOk"
+              style="display:none;">
+              DONE
+            </button>
+            <button
+              class="px-4 py-2 m-6 font-bold text-white bg-red-500 rounded"
+              id="userPasswordKo"
+              style="display:none;">
+              ERROR
+            </button>
+          </div>
+        </section> 
         <section v-if="settingsSection=='privacy'">
           <div class="m-2">Privacy section</div>
         </section> 
         <section v-if="settingsSection=='plan'">
           <div class="m-2">Plan section</div>
-        </section> 
-        <section v-if="settingsSection=='password'">
-          <div class="m-2">Password section</div>
         </section> 
       </div>
     </div>
@@ -118,6 +161,11 @@
           Email: "",
           Twitter: "",
           Website: ""
+        },
+        userPassword: {
+          OldPassword: "",
+          NewPassword: "",
+          RepeatNewPassword: ""
         }
       }
     },
@@ -163,6 +211,26 @@
             var s = document.getElementById("userSettingsOk").style;
           } else {
             var s = document.getElementById("userSettingsKo").style;
+          }
+          s.display = "block";
+          s.opacity = 1;
+          (function fade(){(s.opacity-=.1)<0?s.display="none":setTimeout(fade,100)})();
+        })
+      },
+      saveUserPassword() {
+        axios({
+          method: "POST",
+          headers: {
+            Authorization: "Bearer " + document.cookie,
+            "Access-Control-Allow-Origin": "*",
+          },
+          url: import.meta.env.VITE_ROOT_API + "/update_password",
+          data: this.userPassword
+        }).then(response => {
+          if (response.data == "OK") {
+            var s = document.getElementById("userPasswordOk").style;
+          } else {
+            var s = document.getElementById("userPasswordKo").style;
           }
           s.display = "block";
           s.opacity = 1;
