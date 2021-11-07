b619c4ed78c761ef8b2b7fee34965cf2137efa33
dolphinxyz <robimalco@gmail.com>
2021-11-07 09:39:36 +0100

    start developing auth with web3

---
+++ src/components/ConnectWallet.vue
@@ -0,0 +1,114 @@
+<template>
+  <div>
+    <div class="hidden md:block">
+      <div v-if="errorMessage">
+        {{ errorMessage }}
+      </div>
+      <div v-else>
+        <button v-if="!isConnected"
+          class="inline-block p-2 mr-2 text-gray-800 rounded hover:bg-header-dark"
+          @click="loginMetamask">
+          Connect Wallet
+        </button>
+        <button v-else>
+          {{ userWallet.substring(0, 5) }}...{{ userWallet.slice(-5) }}
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+  declare let window: any;
+  import axios from "axios";
+  import { Component, Vue } from 'vue-property-decorator';
+  import Web3 from "web3";
+  import { set } from 'idb-keyval';
+  import User from '@/store/userModule';
+  import { getModule } from 'vuex-module-decorators'
+  const userStore = getModule(User)
+  @Component({
+    components: {}
+  })
+  export default class LoginMeta extends Vue {
+    isConnected: boolean = false;
+    isAccountChanged: boolean = false;
+    userWallet: string = "";
+    errorMessage: string = "";
+    created() {
+      var self = this;
+      window.addEventListener("load", function() {
+        window.ethereum.on('accountsChanged', function () {
+          self.verifyConnection();
+        });
+        window.ethereum.on('chainChanged', function(){
+          self.verifyConnection();
+        });
+      })
+      this.verifyConnection();
+    }
+    async loginMetamask() {
+      if (window.ethereum) {
+        await window.ethereum.send('eth_requestAccounts');
+      }
+    }
+    async verifyConnection() {
+      window.web3 = new Web3(window.ethereum);
+      let accounts = await window.web3.eth.getAccounts(function(error: any, accounts: any) {
+        if (error) {
+          console.log("Error: ", error);
+        } else {
+          return accounts;
+        }
+      });
+      if (accounts.length > 0) {
+        let chainId = await window.web3.eth.getChainId(function(chainId: any) {
+          return chainId;
+        });
+        if (chainId != 1287) {
+          this.isConnected = false;
+          this.errorMessage = "Wrong network"
+          alert("Please connect to Moonbase Alpha!")
+          return;
+        }
+        this.isConnected = true;
+        this.userWallet = accounts[0];
+        this.errorMessage = ""
+        console.log("SUCCESSFULLY CONNECTED WITH MOONBASE ALPHA:");
+        console.log("\tChainId: ", chainId);
+        console.log("\tAccount: ", accounts[0]);
+        axios({
+          method: "GET",
+          url: process.env.VUE_APP_HTTP_URL + "/login/" + this.userWallet,
+        }).then(response => {
+          if (response.status == 200) {
+            let sessionId: string = response.data['SessionId'];
+            console.log("SESSIONID")
+            console.log(sessionId)
+            if (sessionId) {
+              // Set cookie
+              let d = new Date();
+              d.setTime(d.getTime() + 1000 * 24 * 60 * 60 * 1000);
+              let expires = "expires=" + d.toUTCString();
+              document.cookie = "sessionId=" + sessionId + ";" + expires + "; path=/";
+              // Save user's data in indexeddb
+              set(response.data['SessionId'], response.data);
+              // Save user's info in store
+              userStore.updateUserDetails(response.data);
+              this.$router.push({
+                name: 'UserTrades',
+                params: {
+                  username: response.data['Username']
+                }
+              })
+            }
+          }
+        })
+      } else {
+        console.log("User is not logged in to MetaMask");
+        this.isConnected = false;
+        return
+      }
+    }
+  }
+</script>
