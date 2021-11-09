c79241656e4b4e390f849785fbe4af18fc6ecd73
dolphinxyz <robimalco@gmail.com>
2021-11-09 08:15:15 +0100

    completed auth

--- src/components/ConnectWallet.vue
+++ src/components/ConnectWallet.vue
@@ -1,16 +1,16 @@
 <template>
   <div>
     <div class="hidden md:block">
-      <div v-if="errorMessage">
-        {{ errorMessage }}
-      </div>
-      <div v-else>
-        <button v-if="!isConnected"
+      <div v-if="!userWallet">
+        <button
           class="inline-block p-2 mr-2 text-gray-800 rounded hover:bg-header-dark"
           @click="loginMetamask">
           Connect Wallet
         </button>
-        <button v-else>
+      </div>
+      <div v-else>
+        <ButtonSettings/>
+        <button>
           {{ userWallet.substring(0, 5) }}...{{ userWallet.slice(-5) }}
         </button>
       </div>
@@ -21,35 +21,90 @@
 <script lang="ts">
   declare let window: any;
   import axios from "axios";
-  import { Component, Vue } from 'vue-property-decorator';
   import Web3 from "web3";
-  import { set } from 'idb-keyval';
-  import User from '@/store/userModule';
+  import { Component, Vue } from 'vue-property-decorator';
   import { getModule } from 'vuex-module-decorators'
+  import { get } from 'idb-keyval';
+  import { set } from 'idb-keyval';
+  import { del } from 'idb-keyval';
+  import ButtonSettings from '@/components/ButtonSettings.vue';
+  import User from '@/store/userModule';
   const userStore = getModule(User)
   @Component({
-    components: {}
+    components: {
+      ButtonSettings
+    }
   })
-  export default class LoginMeta extends Vue {
-    isConnected: boolean = false;
-    isAccountChanged: boolean = false;
-    userWallet: string = "";
-    errorMessage: string = "";
-    created() {
+  export default class ConnectWallet extends Vue {
+    get userWallet() {
+      return userStore.getUserDetails['Wallet'];
+    }
+    async created() {
+      if(document.cookie.indexOf("sessionId") > -1) {
+        var sessionId = document.cookie.split("sessionId=")[1].split(";")[0];
+        await get(sessionId).then((val) => userStore.updateUserDetails(val));
+      };
       var self = this;
-      window.addEventListener("load", function() {
-        window.ethereum.on('accountsChanged', function () {
-          self.verifyConnection();
-        });
-        window.ethereum.on('chainChanged', function(){
-          self.verifyConnection();
-        });
-      })
-      this.verifyConnection();
+      window.ethereum.on('accountsChanged', function () {
+        self.verifyConnection();
+      });
+      window.ethereum.on('chainChanged', function(){
+        self.verifyConnection();
+      });
     }
     async loginMetamask() {
       if (window.ethereum) {
         await window.ethereum.send('eth_requestAccounts');
+        window.web3 = new Web3(window.ethereum);
+        let accounts = await window.web3.eth.getAccounts(function(error: any, accounts: any) {
+          if (error) {
+            console.log("Error: ", error);
+          } else {
+            return accounts;
+          }
+        });
+        if (accounts.length > 0) {
+          let chainId = await window.web3.eth.getChainId(function(chainId: any) {
+            return chainId;
+          });
+          if (chainId != 1287) {
+            alert("Please connect to Moonbase Alpha!")
+            return;
+          }
+          axios({
+            method: "GET",
+            url: process.env.VUE_APP_HTTP_URL + "/login/" + accounts[0],
+          }).then(response => {
+            if (response.status == 200) {
+              let sessionId: string = response.data['SessionId'];
+              if (sessionId) {
+                // Set cookie
+                let d = new Date();
+                d.setTime(d.getTime() + 1000 * 24 * 60 * 60 * 1000);
+                let expires = "expires=" + d.toUTCString();
+                document.cookie = "sessionId=" + sessionId + ";" + expires + "; path=/";
+                // Save user's info in store
+                userStore.updateUserDetails(response.data);
+                // Save user's data in indexeddb
+                set(response.data['SessionId'], response.data);
+                // Update local variables
+                console.log("SUCCESSFULLY CONNECTED WITH MOONBASE ALPHA:");
+                console.log("\tChainId: ", chainId);
+                console.log("\tAccount: ", accounts[0]);
+                if (this.$route.params['wallet'] == accounts[0]) {
+                  this.$router.go(0);
+                } else if (this.$route.params['wallet'] != accounts[0]) {
+                  this.$router.push({
+                    name: 'UserTrades',
+                    params: {
+                      wallet: accounts[0] 
+                    }
+                  })
+                }
+              }
+            }
+          })
+        }
       }
     }
     async verifyConnection() {
@@ -61,54 +116,47 @@
           return accounts;
         }
       });
+      console.log(accounts);
       if (accounts.length > 0) {
         let chainId = await window.web3.eth.getChainId(function(chainId: any) {
           return chainId;
         });
         if (chainId != 1287) {
-          this.isConnected = false;
-          this.errorMessage = "Wrong network"
+          this.clean()
           alert("Please connect to Moonbase Alpha!")
           return;
-        }
-        this.isConnected = true;
-        this.userWallet = accounts[0];
-        this.errorMessage = ""
-        console.log("SUCCESSFULLY CONNECTED WITH MOONBASE ALPHA:");
-        console.log("\tChainId: ", chainId);
-        console.log("\tAccount: ", accounts[0]);
-        axios({
-          method: "GET",
-          url: process.env.VUE_APP_HTTP_URL + "/login/" + this.userWallet,
-        }).then(response => {
-          if (response.status == 200) {
-            let sessionId: string = response.data['SessionId'];
-            console.log("SESSIONID")
-            console.log(sessionId)
-            if (sessionId) {
-              // Set cookie
-              let d = new Date();
-              d.setTime(d.getTime() + 1000 * 24 * 60 * 60 * 1000);
-              let expires = "expires=" + d.toUTCString();
-              document.cookie = "sessionId=" + sessionId + ";" + expires + "; path=/";
-              // Save user's data in indexeddb
-              set(response.data['SessionId'], response.data);
-              // Save user's info in store
-              userStore.updateUserDetails(response.data);
-              this.$router.push({
-                name: 'UserTrades',
-                params: {
-                  username: response.data['Wallet']
-                }
-              })
-            }
+        } else {
+          if (this.$route.params['wallet'] != accounts[0]) {
+            this.$router.push({
+              name: 'UserTrades',
+              params: {
+                wallet: accounts[0] 
+              }
+            })
+          } else {
+            this.$router.go(0);
           }
-        })
+        }
       } else {
-        console.log("User is not logged in to MetaMask");
-        this.isConnected = false;
+        this.clean();
         return
       }
+    }
+    clean() {
+      // Reset indexeddb
+      if(document.cookie.indexOf("sessionId") > -1) {
+        var sessionId = document.cookie.split("sessionId=")[1].split(";")[0];
+        del(sessionId);
+      }
+      // Reset cookie
+      const date = new Date();
+      date.setTime(date.getTime() + (-1 * 24 * 60 * 60 * 1000));
+      document.cookie = "sessionId=; expires="+date.toUTCString()+"; path=/";
+      // Reset user store
+      userStore.updateUserDetails({});
+      // Reset local variables
+      // Refresh page
+      this.$router.go(0);
     }
   }
 </script>
--- src/router/index.ts
+++ src/router/index.ts
@@ -2,13 +2,17 @@ import Vue from 'vue'
 import VueRouter, { RouteConfig } from 'vue-router'
 Vue.use(VueRouter)
 
+declare let window: any;
+import Web3 from "web3";
+
 import { get } from 'idb-keyval';
+import { del } from 'idb-keyval';
+
 import User from '@/store/userModule';
 import { getModule } from 'vuex-module-decorators'
 const userStore = getModule(User)
 
 import Home from '../views/Home.vue'
-import Login from '../views/Login.vue'
 import UserTrades from '../views/UserTrades.vue'
 import Settings from '../views/Settings.vue'
 
@@ -17,33 +21,14 @@ const routes: Array<RouteConfig> = [
     path: '/',
     name: 'Home',
     component: Home,
-    beforeEnter: (to, from, next) => {
-      if(document.cookie.indexOf("sessionId") > -1) {
+    async beforeEnter ({}, {}, next) {
+      if (await isAllowedToGoNext()) {
         var sessionId = document.cookie.split("sessionId=")[1].split(";")[0];
-        get(sessionId).then((sessionData) => 
-          next('/' + sessionData['Wallet'])
-        )
+        await get(sessionId).then((val) => userStore.updateUserDetails(val));
+        let sessionData = userStore.getUserDetails;
+        next('/' + sessionData['Wallet']);
       } else {
         next()
-      }
-    }
-  },
-  {
-    path: '/login',
-    name: 'Login',
-    component: Login
-  },
-  {
-    path: '/settings',
-    name: 'Settings',
-    component: Settings,
-    async beforeEnter (to, from, next) {
-      if(document.cookie.indexOf("sessionId") > -1) {
-        var session_id = document.cookie.split("sessionId=")[1].split(";")[0];
-        await get(session_id).then((val) => userStore.updateUserDetails(val));
-        next()
-      } else {
-        next('/login')
       }
     }
   },
@@ -51,18 +36,70 @@ const routes: Array<RouteConfig> = [
     path: '/:wallet',
     name: 'UserTrades',
     component: UserTrades,
-    async beforeEnter (to, from, next) {
-      if(document.cookie.indexOf("sessionId") > -1) {
-        var session_id = document.cookie.split("sessionId=")[1].split(";")[0];
-        await get(session_id).then((val) => userStore.updateUserDetails(val));
+    async beforeEnter ({}, {}, next) {
+      if (await isAllowedToGoNext()) {
+        var sessionId = document.cookie.split("sessionId=")[1].split(";")[0];
+        await get(sessionId).then((val) => userStore.updateUserDetails(val));
+        next();
+      } else {
+        next();
+      }
+    }
+  },
+  {
+    path: '/settings',
+    name: 'Settings',
+    component: Settings,
+    async beforeEnter ({}, {}, next) {
+      if (await isAllowedToGoNext()) {
         next()
       } else {
-        next()
+        next('/');
       }
-      next()
     }
-  }
+  },
 ]
+
+async function isAllowedToGoNext() {
+  if(document.cookie.indexOf("sessionId") > -1) {
+    let accounts = await loadAccounts();
+    if (accounts.length > 0) {
+      return true
+    } else {
+      clean();
+      return false
+    }
+  } else {
+    return false
+  }
+}
+
+async function loadAccounts() {
+  window.web3 = new Web3(window.ethereum);
+  let accounts = await window.web3.eth.getAccounts(function(error: any, accounts: any) {
+    if (error) {
+      console.log("Error: ", error);
+    } else {
+      return accounts;
+    }
+  })
+  return accounts;
+}
+
+function clean() {
+  // Reset indexeddb
+  if(document.cookie.indexOf("sessionId") > -1) {
+    var sessionId = document.cookie.split("sessionId=")[1].split(";")[0];
+    del(sessionId);
+  }
+  // Reset cookie
+  const date = new Date();
+  date.setTime(date.getTime() + (-1 * 24 * 60 * 60 * 1000));
+  document.cookie = "sessionId=; expires="+date.toUTCString()+"; path=/";
+  // Reset user store
+  userStore.updateUserDetails({});
+}
+
 
 const router = new VueRouter({
   mode: 'history',
