3abffea65c8a96e5f6cfcf3ccf53895f14fde636
dolphinxyz <robimalco@gmail.com>
2021-11-16 21:32:59 +0100

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,16 @@
-module: {
-rules: [
-  {
-    test: /\.js$/,
-    enforce: 'pre',
-    use: ['source-map-loader'],
+module.exports = {
+  devtool: 'eval-source-map',
+  use: ['source-map-loader'],
+  module: {
+    loaders: [
+      {
+        test: /.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
   },
-],
+};
