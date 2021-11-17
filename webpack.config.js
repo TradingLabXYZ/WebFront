db21450be8cba0fdcd318e3b9a03dc9a09c6b132
dolphinxyz <robimalco@gmail.com>
2021-11-17 11:10:13 +0100

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,4 @@
 module.exports = {
-  devtool: 'eval-source-map',
-  use: ['source-map-loader'],
-  module: {
-    loaders: [
-      {
-        test: /.jsx?$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/,
-        query: {
-          presets: ['es2015', 'react']
-        }
-      }
-    ]
-  },
+  devtool: 'eval-cheap-source-map',
+  use: ['source-map-loader']
 };
