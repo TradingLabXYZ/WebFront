5714225f9e9e1c5750c8685dbac760b9d8d42d53
dolphinxyz <robimalco@gmail.com>
2021-12-11 09:02:17 +0100

    #34 first attemp

---
+++ src/components/landing/Footer.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="flex flex-col justify-center py-16 text-center bg-header space-y-2">
+    <div class="flex flex-row justify-center">
+      <Discord class="transform scale-75"/>
+      <Twitter class="transform scale-75"/>
+      <Github class="transform scale-75"/>
+    </div>
+    <div class="text-2xl font-semibold">
+      TradingLab
+    </div>
+    <div class="font-normal text-md">
+      Copyright © tradinglab.xyz 2021
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+  import { Vue, Component } from 'vue-property-decorator';
+  import Discord from '@/components/svg/Discord.vue';
+  import Twitter from '@/components/svg/Twitter.vue';
+  import Github from '@/components/svg/Github.vue';
+  @Component({
+    components: {
+      Discord,
+      Twitter,
+      Github
+    }
+  })
+  export default class Header extends Vue {}
+</script>
---
+++ src/components/landing/Hero.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <div class="flex justify-center text-center h-fit bg-header">
+      <div class="py-52">
+        <div class="p-2 text-4xl tracking-widest text-homeherotext">
+          <span class="font-medium">H</span>
+          <span class="italic">y</span>
+          <span class="font-serif font-semibold">p</span>
+          <span class="italic font-extralight">e</span>
+          <span class="font-bold">r</span>
+          <span> </span>
+          <span class="font-extralight text-md">H</span>
+          <span class="font-serif">y</span>
+          <span class="font-black">b</span>
+          <span class="italic font-extralight">r</span>
+          <span class="font-mono">i</span>
+          <span class="">d</span>
+        </div>
+        <div class="p-2 text-5xl tracking-widest text-homeherotext">Crypto Trading Community</div>
+        <div class="p-2 text-xl font-extralight">Powered by Polkadot</div>
+        <button class="inline-block px-10 py-2 mt-6 text-xl font-bold text-white rounded bg-homebutton">
+          Explore
+        </button>
+      </div>
+    </div>
+    <div class="flex justify-around py-16 text-4xl font-extrabold h-fit bg-tradenew text-hometextband">
+      <div class="">
+        Decentralised
+      </div>
+      <div class="">
+        Open Source
+      </div>
+      <div class="">
+        Innovative
+      </div>
+    </div>
+  </div>
+</template>
