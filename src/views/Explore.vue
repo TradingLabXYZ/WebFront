71f044e82af6a12e141fa4867687a692a5ec8158
dolphinxyz <robimalco@gmail.com>
2021-12-15 09:20:26 +0100

    #70 ready, missing mobile

--- src/views/Explore.vue
+++ src/views/Explore.vue
@@ -1,86 +1,92 @@
 <template>
   <div>
     <Header/>
-    <div class="mt-5 mb-5 text-center xs:text-xl sm:text-2xl">Explore</div>
+    <div class="mt-5 mb-3 text-center xs:text-xl sm:text-2xl">
+      Explore
+    </div>
     <div class="flex flex-col">
       <div
         v-for="event in events"
         :key="event.createdat"
-        class="grid grid-cols-12 divide-y-2 divide-gray-300">
+        class="my-4 grid grid-cols-11">
         <div class="col-span-4"></div>
-        <div v-if="event.eventtype=='trade'" class="py-12 text-center col-span-4">
+        <div class="flex flex-col text-center border-t border-b border-gray-200 col-span-3">
           <router-link :to="'/' + event.userwallet">
-            <div class="flex flex-row items-center justify-center align-middle space-x-5">
-              <img  
-                :src="event.profilepicture"
-                class="rounded-full h-14">
-              <div class="text-left">
-                <div class="flex flex-row items-center align-middle space-x-1">
-                  <div class="text-lg font-bold">
-                    New trade
-                  </div>
-                  <div class="">
-                    on {{ event.exchange }}
-                  </div>
+            <div class="flex flex-row justify-between">
+              <div class="flex flex-row items-center justify-start mt-1 align-middle space-x-1">
+                <img
+                  :src="event.profilepicture"
+                  height="30" width="30"
+                  class="rounded-full">
+                <span class="font-light text-gray-700 text-md">
+                  {{ event.type }} {{ event.quantity }} {{ event.secondpairsymbol }}
+                </span>
+              </div>
+              <div class="flex items-center mt-1 text-sm italic font-light text-gray-700 align-middle">
+                "{{ event.reason }}"
+              </div>
+              <div class="flex flex-row items-center justify-end align-middle">
+                <span class="font-light text-gray-700 text-md">
+                  {{ event.firstpairsymbol }}/{{ event.secondpairsymbol }}
+                </span>
+                <img
+                  :src="event.firstpairurlicon"
+                  class="relative border rounded-full left-1"
+                  height="20"
+                  width="20">
+                <img
+                  :src="event.secondpairurlicon"
+                  class="border rounded-full"
+                  height="20"
+                  width="20">
+              </div>
+            </div>
+            <div class="flex flex-row items-center mt-2 mb-4 align-middle justify-evenly">
+              <div class="flex flex-col text-center">
+                <div class="text-gray-400 text-xxs">
+                  Avg Price {{ event.firstpairsymbol }}/{{ event.secondpairsymbol }}
                 </div>
-                <div class="text-xs italic text-gray-500">
-                  {{ event.userwallet }}
-                </div>
-                <div class="text-xs italic text-gray-500">
-                {{ event.timeago }}
+                <div class="text-lg leading-3">
+                  {{ event.avgprice }}
                 </div>
               </div>
-              <div class="flex flex-row items-center justify-center text-center align-middle space-x-5">
-                <div class="">
-                  <img
-                    :src="event.firstpairurlicon"
-                    height="30px" width="30px">
-                  <div class="text-gray-500 text-xxs">
-                    {{ event.firstpairsymbol }}
-                  </div>
+              <div class="flex flex-col text-center">
+                <div class="text-gray-400 text-xxs">
+                  Total {{ event.firstpairsymbol }}
                 </div>
-                <div>
-                  <img
-                    :src="event.secondpairurlicon"
-                    height="30px" width="30px">
-                  <div class="text-gray-500 text-xxs">
-                    {{ event.secondpairsymbol }}
+                <div class="text-lg leading-3">
+                  {{ event.total }}
+                </div>
+              </div>
+              <div class="flex flex-col text-center">
+                <div class="text-gray-400 text-xxs">
+                  Current Price {{ event.firstpairsymbol }}/{{ event.secondpairsymbol }}
+                </div>
+                <div class="flex flex-row items-start justify-center align-middle space-x-1">
+                  <div class="text-lg leading-3">
+                    {{ event.currentprice }}
+                  </div>
+                  <div
+                    v-if="event.type=='BUY'"
+                    class="text-xs"
+                    :class="event.deltapriceperc > 0 ? 'text-tradepositive' : 'text-tradenegative'">
+                    {{ event.deltapriceperc + "%" }}
+                  </div>
+                  <div
+                    v-else
+                    class="text-xs"
+                    :class="event.deltapriceperc > 0 ? 'text-tradenegative' : 'text-tradepositive'">
+                    {{ event.deltapriceperc + "%" }}
                   </div>
                 </div>
               </div>
             </div>
-          </router-link>
-        </div>
-        <div v-else class="py-12 text-center col-span-4">
-          <router-link :to="'/' + event.userwallet">
-            <div class="flex flex-row items-center justify-center align-middle space-x-8">
-              <img
-                :src="event.profilepicture"
-                class="rounded-full h-14">
-              <div class="text-left">
-                <div class="flex flex-row items-center align-middle space-x-1">
-                  <div class="font-bold">
-                    {{ event.type }}
-                  </div>
-                  <div class="">
-                    {{ event.quantity }}
-                  </div>
-                  <div class="">
-                    <img
-                      :src="event.secondpairurlicon"
-                      class="inline-block"
-                      height="30px" width="30px">
-                  </div>
-                  <div class="">
-                      {{ event.secondpairsymbol }}
-                  </div>
-                  <div class="">
-                    {{ event.timeago }}
-                  </div>
-                </div>
-                <div class="text-xs italic text-gray-500">
-                  {{ event.userwallet }}
-                </div>
+            <div class="flex flex-row justify-between text-gray-400 text-xxs">
+              <div>
+                {{ event.userwallet }}
+              </div>
+              <div>
+                {{ event.timeago }}
               </div>
             </div>
           </router-link>
@@ -119,7 +125,6 @@
         url: request_url,
       }).then(response => {
         if (response.status === 200) {
-          console.log(response.data);
           this.events = response.data;
         }
       }).catch(function (error) {
