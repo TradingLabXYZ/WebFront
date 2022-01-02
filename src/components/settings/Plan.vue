123dc42fb9b0f4fdc0dbfbf2baa9c626d7f5ea03
dolphinxyz <robimalco@gmail.com>
2022-01-02 19:13:21 +0100

    #126 testing new function

--- src/components/settings/Plan.vue
+++ src/components/settings/Plan.vue
@@ -1,27 +1,49 @@
 <template>
   <div>
-    <div v-if="usdcMonthlyPrice > 0" class="p2">
-      Your actual USDC monthly price: {{ usdcMonthlyPrice }}
+    <div class="mb-10">
+      <div v-if="usdcMonthlyPrice > 0" class="p2">
+        Your actual USDC monthly price: {{ usdcMonthlyPrice }}
+      </div>
+      <div v-else>
+        You do not have set yet your USDC monthly price.
+      </div>
+      <div class="p2">
+      <div class="mt-6 p2">
+        Change your USDC monthly price
+        <input
+          type="text"
+          class="w-full p-2 text-gray-800 border border-gray-200 border-gray-500"
+          v-model="newUsdcMonthlyPrice"
+          placeholder="Plan Type">
+      </div>
+      </div>
+      <button
+        class="px-4 py-2 m-6 font-bold text-white rounded bg-azure hover:bg-deeplagune"
+        type="submit"
+        @click="changePlan()">
+        Save
+      </button>
     </div>
-    <div v-else>
-      You do not have set yet your USDC monthly price.
+    <div class="mb-10">
+      <div>
+        Your actual ETH ADDRESS: {{ receivedEthAddress }}
+      </div>
+      <div class="mt-6 p2">
+        Your random ETH addres
+        <input
+          type="text"
+          class="w-full p-2 text-gray-800 border border-gray-200 border-gray-500"
+          v-model="newEthAddress"
+          placeholder="ETH address">
+      </div>
+      </div>
+      <button
+        class="px-4 py-2 m-6 font-bold text-white rounded bg-azure hover:bg-deeplagune"
+        type="submit"
+        @click="subscribe()">
+        Save
+      </button>
     </div>
-    <div class="p2">
-    <div class="mt-6 p2">
-      Change your USDC monthly price
-      <input
-        type="text"
-        class="w-full p-2 text-gray-800 border border-gray-200 border-gray-500"
-        v-model="newUsdcMonthlyPrice"
-        placeholder="Plan Type">
-    </div>
-    </div>
-    <button
-      class="px-4 py-2 m-6 font-bold text-white rounded bg-azure hover:bg-deeplagune"
-      type="submit"
-      @click="changePlan()">
-      Save
-    </button>
   </div>
 </template>
 
@@ -37,6 +59,8 @@
   export default class Privacy extends Vue {
     usdcMonthlyPrice: string = "";
     newUsdcMonthlyPrice: string = "";
+    newEthAddress: string = "";
+    receivedEthAddress: string = "";
     key: string = "";
     value: string = "";
     async created() {
@@ -49,6 +73,9 @@
           console.log(this.usdcMonthlyPrice);
         }
       });
+      contractStore.getContractSubscription.on("Subscribe", (from, to, {}) => {
+        console.log(from, to);
+      });
     }
     async getNumber() {
       let tempValue = await contractStore.getContractSubscription.getPlan();
@@ -57,6 +84,9 @@
     }
     async changePlan() {
       await contractStore.getContractSubscriptionSigned.changePlan(this.newUsdcMonthlyPrice);
+    }
+    async subscribe() {
+      await contractStore.getContractSubscriptionSigned.subscribe(this.newEthAddress);
     }
   }
 </script>
