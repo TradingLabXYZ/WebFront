62ff7cb5dcb075bf3aa0977b700434f0a4eb42c1
dolphinxyz <robimalco@gmail.com>
2022-01-28 11:05:00 +0100

    #38 basic example walletconnect

--- src/views/Test.vue
+++ src/views/Test.vue
@@ -1,7 +1,6 @@
 <template>
   <div>
     <div>
-      IS CONNECTED: {{ isConnected }}
     </div>
     <div>
       <button
@@ -21,79 +20,29 @@
 <script lang="ts">
   import { Component, Vue } from 'vue-property-decorator';
   import { ethers } from "ethers";
-  import Web3Modal from 'web3modal';
-  import { providerOptions } from '@/web3/config';
-  /* import WalletConnectProvider from "@walletconnect/web3-provider"; */
-  import { WalletConnectConnector } from "@web3-react/walletconnect-connector";
-
-  import { walletconnect } from "@/web3/test"
-
+  import WalletConnectProvider from '@walletconnect/ethereum-provider'
   @Component({
     components: {},
   })
-
   export default class Test extends Vue {
-    isConnected = false;
-    web3Modal = new Web3Modal({
-      theme: 'dark',
-      providerOptions,
-    });
-
     async created() {
-
-        /* var walletconnect = new WalletConnectConnector({
-          rpc: {
-              1287: 'https://rpc.api.moonbase.moonbeam.network'
-          },
-          bridge: 'https://bridge.walletconnect.org',
-          qrcode: true
-        })
-        
-        let { provider } = await walletconnect.activate();
-        
-        const signer = provider.getSigner();
- */
-
-      /* let { provider } = await walletconnect.activate(); */
-      is:open is:issue 
-      /*const signer = provider.getSigner();
-
-      const mamma = new ethers.providers.Web3Provider(signer);
-
-
-
-      /* const provider = new ethers.providers.Web3Provider(walletconnect);
-      await provider.getNetwork().then(function(network) {console.log(network)}); */
-
-
-      const instance = await this.web3Modal.connect();
-     /* const provider = new ethers.providers.Web3Provider(instance);
-      await provider.getNetwork().then(function(network) {console.log(network)}); */
-
-      /* instance.on('accountsChanged', async (accounts: object) => {
-        console.log("ACCOUNT CHANGED", accounts);
+      const provider = new WalletConnectProvider({
+        rpc: {
+            1287: "https://rpc.api.moonbase.moonbeam.network",
+        },
+        qrcode: true
       });
-      instance.on('chainChanged', async (chainId: string) => {
-        console.log("CHAIN CHANGED", chainId);
-      }); */
-
-      /* await provider.getNetwork().then(function(network) {console.log(network)}); */
-      /* if (localStorage.getItem("WEB3_CONNECT_CACHED_PROVIDER")) {
-        this.isConnected = true;
-      } */
+      await provider.enable();
+      const web3Provider = new ethers.providers.Web3Provider(provider);
+      var address = await web3Provider.listAccounts().then(function(addresses) {return addresses[0]});
+      console.log("HERE THE ADDRESS", address);
+      provider.on("accountsChanged", (accounts: string[]) => {
+        console.log(accounts);
+      });
     }
-
     async handleWalletConnect() {
-      /* const instance = await this.web3Modal.connect();
-      const provider = new ethers.providers.Web3Provider(instance);
-      const signer = provider.getSigner();
-      await provider.getNetwork().then(function(network) {console.log(network)});
-      await signer.getBalance().then(function(balance) {console.log(balance.toString())});
-      this.isConnected = true; */
     };
     async resetApp() {
-      /* this.web3Modal.clearCachedProvider();
-      this.isConnected = false; */
     };
   }
 </script>
