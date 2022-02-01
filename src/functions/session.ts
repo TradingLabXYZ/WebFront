2db47c50cbb8390249e7bda3c4092ac602826f77
dolphinxyz <robimalco@gmail.com>
2022-02-01 16:08:45 +0100

    #138 reorganized the login code

---
+++ src/functions/session.ts
@@ -0,0 +1,52 @@
+import { clear } from 'idb-keyval';
+import { getModule } from 'vuex-module-decorators'
+import User from '@/store/userModule';
+const userStore = getModule(User)
+import Wallet from '@/store/walletModule';
+const walletStore = getModule(Wallet)
+import axios from "axios";
+import { set } from 'idb-keyval';
+
+export function cleanSession() {
+  // Reset indexeddb
+  clear()
+  // Reset cookie
+  const date = new Date();
+  date.setTime(date.getTime() + (-1 * 24 * 60 * 60 * 1000));
+  document.cookie = "sessionId=; expires="+date.toUTCString()+"; path=/";
+  // Reset user store
+  userStore.updateUserDetails({});
+  // Reset wallet store
+  walletStore.updateIsConnected(false);
+  walletStore.updateBalance(0);
+  walletStore.updateChainId(0);
+  walletStore.updateWallet('');
+}
+
+export function isAllowedToGoNext(): boolean {
+  if(document.cookie.indexOf("sessionId") > -1) {
+    return true
+  } else {
+    cleanSession();
+    return false
+  }
+}
+
+export async function generateSession(account: string) {
+  let api_url = process.env.VUE_APP_HTTP_URL + '/login/' + account;
+  const response = await axios.get(api_url);
+  if (response.status != 200) {
+    return
+  }
+  let sessionId: string = response.data['SessionId'];
+  // Set cookie
+  let d = new Date();
+  d.setTime(d.getTime() + 1000 * 24 * 60 * 60 * 1000);
+  let expires = "expires=" + d.toUTCString();
+  document.cookie = "sessionId=" + sessionId + ";" + expires + "; path=/";
+  // Save user's info in store
+  userStore.updateUserDetails(response.data);
+  // Save user's data in indexeddb
+  set(response.data['SessionId'], response.data);
+}
+
