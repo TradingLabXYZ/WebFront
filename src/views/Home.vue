4fc74b806bd0c5c1af3957d09d21b59080eb7213
dolphinxyz <robimalco@gmail.com>
2022-02-12 11:36:38 +0100

    #152 close

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,25 +2,28 @@
   <div>
     <Header/>
     <Hero/>
-    <div class="">
-      <svg height="80" viewBox="0 0 500 80" preserveAspectRatio="none" class="fill-current text-blueshine">
-        <path d="M0,0 L0,80 Q0,80 500,0 L500,0"/>
-      </svg>
-    </div>
+    <svg height="80" viewBox="0 0 500 80" preserveAspectRatio="none" class="fill-current text-blueshine">
+      <path d="M0,0 L0,80 500,0"/>
+    </svg>
     <Share/>
-    <div class="">
-      <svg height="80" viewBox="0 0 500 80" preserveAspectRatio="none" class="fill-current text-azure">
-        <path d="M0,0 C0,80 500,80 500,0"/>
-      </svg>
-    </div>
+    <svg height="80" viewBox="0 0 500 80" preserveAspectRatio="none" class="fill-current text-azure">
+      <path d="M0,0 C0,80 500,80 500,0"/>
+    </svg>
     <Features/>
-    <div class="">
-      <svg height="80" viewBox="0 0 500 80" preserveAspectRatio="none" class="fill-current text-magentashine">
-        <path d="M0,0 C150,30 350,30 500,0"/>
-      </svg>
-    </div>
+    <svg height="80" viewBox="0 0 500 80" preserveAspectRatio="none" class="fill-current text-magentashine">
+      <path d="M0,0 C150,30 350,30 500,0"/>
+    </svg>
     <Privacy1/>
     <Privacy2/>
+    <svg height="80" viewBox="0 0 500 80" preserveAspectRatio="none" class="fill-current text-lagune">
+      <path d="M0,0 L0,80 500,0"/>
+      <path d="M0,0 L500,0 500,80"/>
+    </svg>
+    <LearnEarn/>
+    <svg height="80" viewBox="0 0 500 80" preserveAspectRatio="none" class="fill-current text-deeplagune">
+      <path d="M0,0 L0,80 500,0"/>
+    </svg>
+    <More/>
     <Footer/>
   </div>
 </template>
@@ -33,6 +36,8 @@
   import Features from '@/components/landing/Features.vue';
   import Privacy1 from '@/components/landing/Privacy1.vue';
   import Privacy2 from '@/components/landing/Privacy2.vue';
+  import LearnEarn from '@/components/landing/LearnEarn.vue';
+  import More from '@/components/landing/More.vue';
   import Footer from '@/components/landing/Footer.vue';
 
   @Component({
@@ -43,6 +48,8 @@
       Features,
       Privacy1,
       Privacy2,
+      LearnEarn,
+      More,
       Footer
     },
   })
