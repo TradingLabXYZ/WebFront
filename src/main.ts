153a5c92005d803117dfc1eed697cd1ef78360f6
dolphinxyz <robimalco@gmail.com>
2022-02-21 11:12:06 +0100

    #177 initial config

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import VueMatomo from 'vue-matomo'
 import '@/assets/css/tailwind.css'
 Vue.config.productionTip = true;
 Vue.config.devtools = true;
 Vue.config.performance = true;
+Vue.use(VueMatomo, {
+  host: "https://analytics.tradinglab.xyz",
+  siteId: 1,
+  router: router
+});
 new Vue({
   store,
   router,
