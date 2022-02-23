597cf1c030215133cda59497d3e2a55056bc8093
dolphinxyz <robimalco@gmail.com>
2022-02-23 11:29:27 +0100

    #185 small update

--- src/components/landing/Survey.vue
+++ src/components/landing/Survey.vue
@@ -1,17 +1,17 @@
 <template>
   <div v-if="isSectionVisible" class="bg-green-200">
     <div class="flex items-center justify-center text-center align-middle xs:text-xs sm:text-sm">
-      Do you have a spare minute to help us?
+      A minute to help us?
       <a
         href="https://docs.google.com/forms/d/e/1FAIpQLScDVra6sDmsA7Vf6s9sp55gH1D5Wjd0rBK9w6KGBi3qnsxejg/viewform"
         target="_blank">
-        <button class="font-bold text-blue-800 bg-blue-200 border border-blue-600 rounded xs:m-1 sm:p-2 sm:m-2 hover:bg-blue-400 xs:text-xs sm:text-sm">
+        <button class="font-bold text-blue-800 bg-blue-200 border border-blue-600 rounded xs:m-1 xs:p-1 sm:p-2 sm:m-2 hover:bg-blue-400 xs:text-xs sm:text-sm">
           Take me to the survey
         </button>
       </a>
       <button
         v-on:click="toggleVisibility"
-        class="ml-12">
+        class="sm:ml-12 xs:ml-4">
         <CloseX/>
       </button>
     </div>
