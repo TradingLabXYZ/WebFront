a39a3b7960850c2d0def821929cf386dbc439576
dolphinxyz <robimalco@gmail.com>
2022-03-18 20:22:25 +0100

    #204 mobile

--- src/components/landing/Develop.vue
+++ src/components/landing/Develop.vue
@@ -1,13 +1,13 @@
 <template>
-  <div class="flex flex-col items-center -mt-20 align-middle bg-universe text-cream pb-44 space-y-10">
-    <div class="flex flex-row pt-40 font-medium xs:text-4xl sm:text-6xl xs:space-x-4 sm:space-x-8">
+  <div class="flex flex-col items-center -mt-20 align-middle bg-universe text-cream pb-44">
+    <div class="pt-40 mt-5 font-medium text-center xs:text-4xl sm:text-6xl xs:space-x-4 sm:space-x-8">
       Develop on TradingLab
     </div>
-    <div class="flex items-center justify-center mt-5 xs:text-lg sm:text-2xl">
+    <div class="flex items-center justify-center mt-12 xs:text-lg sm:text-2xl xs:mx-12 xs:text-center">
       Extend TradingLab's functionalities with our powerful API<br>
       and integrate pretty much anything you can think about.
     </div>
-    <div class="flex flex-col items-center justify-center align-middle">
+    <div class="flex flex-col items-center justify-center mt-12 align-middle">
       <img src="@/assets/api.gif" alt="Rack" class="w-5/6 xs:pb-12 sm:pb-10">
       <button class="text-2xl font-bold rounded w-60 xs:p-1 sm:p-3 bg-blueshine hover:bg-magentashine">
         <router-link to="/docs">
