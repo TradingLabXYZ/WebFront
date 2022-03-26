b4124d4fbe2748a618f52d891745bfb8ea84587a
dolphinxyz <robimalco@gmail.com>
2022-03-26 19:29:21 +0100

    #222 add metafora

---
+++ src/components/svg/Metafora.vue
@@ -0,0 +1,12 @@
+<template>
+  <svg width="50" height="40" viewBox="0 0 160 160" class="fill-current text-cream">
+    <ellipse cx="27.5" cy="69" rx="27.5" ry="27"/>
+    <path d="M68.113 28.565C68.253 12.718 81.171 0 97.02 0H144c16.016 0 29 12.984 29 29v10.835c0 15.908-12.808 28.845-28.715 28.957-30.725.216-75.181.416-75.838-.234-.556-.549-.497-21.423-.334-39.993Z"/>
+  </svg>
+</template>
+
+<script lang="ts">
+  import { Vue, Component } from 'vue-property-decorator';
+  @Component({})
+  export default class Metafora extends Vue {}
+</script>
