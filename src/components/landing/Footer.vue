58718e5c10bcfd82ac0a83958390ce91fb30d231
dolphinxyz <robimalco@gmail.com>
2022-03-27 10:20:03 +0200

    #222 close

--- src/components/landing/Footer.vue
+++ src/components/landing/Footer.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex flex-col justify-center pb-20 -mt-20 text-center pt-44 bg-gradient-to-br from-universelight via-universe to-universe space-y-4">
-    <div class="flex flex-row justify-center">
+    <div class="flex flex-row justify-center xs:space-x-2 sm:space-x-6">
       <button>
         <a href="https://discord.gg/PEEUfrbQ9c" target="_blank">
           <Discord class="transform scale-75"/>
@@ -17,13 +17,13 @@
         </a>
       </button>
       <button>
-        <a href="https://www.youtube.com/channel/UC0MysGuteFUSLrTpzSHdoIg" target="_blank">
-          <Youtube class="transform scale-75"/>
+        <a href="https://www.metafora.app/tradinglab" target="_blank">
+          <Metafora class="transform scale-75"/>
         </a>
       </button>
       <button>
-        <a href="https://www.metafora.app/tradinglab" target="_blank">
-          <Metafora class="transform scale-75"/>
+        <a href="https://www.youtube.com/channel/UC0MysGuteFUSLrTpzSHdoIg" target="_blank">
+          <Youtube class="transform scale-75"/>
         </a>
       </button>
     </div>
@@ -52,8 +52,8 @@ import Metafora from '@/components/svg/Metafora.vue';
     Twitter,
     Github,
     Youtube,
-    Metafora
-  }
+    Metafora,
+  },
 })
 export default class Footer extends Vue {}
 </script>
--- src/components/svg/Metafora.vue
+++ src/components/svg/Metafora.vue
@@ -1,10 +1,8 @@
 <template>
-  <svg width="80" height="30" viewBox="0 0 300 100" class="fill-current text-cream">
-    <ellipse cx="27.5" cy="69" rx="27.5" ry="27"/>
-    <path d="M68.113 28.565C68.253 12.718 81.171 0 97.02 0H144c16.016 0 29 12.984 29 29v10.835c0 15.908-12.808 28.845-28.715 28.957-30.725.216-75.181.416-75.838-.234-.556-.549-.497-21.423-.334-39.993Z"/>
+  <svg width="80" height="30" viewBox="0 0 125 69" class="fill-current text-cream">
+  <path d="M38 50a19 19 0 1 1-19-19 19 19 0 0 1 19 19Zm67-50H70a20 20 0 0 0-20 20v30h55a20 20 0 0 0 20-20V20a20 20 0 0 0-20-20Z"/>
   </svg>
 </template>
-
 <script lang="ts">
 import {Vue, Component} from 'vue-property-decorator';
 @Component({})
