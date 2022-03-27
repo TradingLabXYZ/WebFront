e93d24dfe0f34486e964126dd59a5e901bf02964
dolphinxyz <robimalco@gmail.com>
2022-03-27 09:57:12 +0200

    #222 close

--- src/components/landing/Footer.vue
+++ src/components/landing/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-col justify-center pb-20 -mt-20 text-center pt-44 bg-gradient-to-br from-universelight via-universe to-universe space-y-2">
+  <div class="flex flex-col justify-center pb-20 -mt-20 text-center pt-44 bg-gradient-to-br from-universelight via-universe to-universe space-y-4">
     <div class="flex flex-row justify-center">
       <button>
         <a href="https://discord.gg/PEEUfrbQ9c" target="_blank">
@@ -21,9 +21,17 @@
           <Youtube class="transform scale-75"/>
         </a>
       </button>
+      <button>
+        <a href="https://www.metafora.app/tradinglab" target="_blank">
+          <Metafora class="transform scale-75"/>
+        </a>
+      </button>
     </div>
-    <div class="text-2xl font-semibold text-magentashine">
-      TradingLab
+    <div class="flex flex-row justify-center space-x-2">
+      <img src="@/assets/logo.png" class="h-8" alt="Logo">
+      <div class="text-2xl font-semibold text-magentashine">
+        TradingLab
+      </div>
     </div>
     <div class="font-normal text-cream text-md">
       Copyright © tradinglab.xyz 2022
@@ -32,18 +40,20 @@
 </template>
 
 <script lang="ts">
-  import { Vue, Component } from 'vue-property-decorator';
-  import Discord from '@/components/svg/Discord.vue';
-  import Twitter from '@/components/svg/Twitter.vue';
-  import Github from '@/components/svg/Github.vue';
-  import Youtube from '@/components/svg/Youtube.vue';
-  @Component({
-    components: {
-      Discord,
-      Twitter,
-      Github,
-      Youtube
-    }
-  })
-  export default class Footer extends Vue {}
+import {Vue, Component} from 'vue-property-decorator';
+import Discord from '@/components/svg/Discord.vue';
+import Twitter from '@/components/svg/Twitter.vue';
+import Github from '@/components/svg/Github.vue';
+import Youtube from '@/components/svg/Youtube.vue';
+import Metafora from '@/components/svg/Metafora.vue';
+@Component({
+  components: {
+    Discord,
+    Twitter,
+    Github,
+    Youtube,
+    Metafora
+  }
+})
+export default class Footer extends Vue {}
 </script>
