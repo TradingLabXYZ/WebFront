c1c6947d23864fa36ae7c8218210d58a9c3865f5
dolphinxyz <robimalco@gmail.com>
2022-04-10 18:13:08 +0200

    #230 starting developing landing page

---
+++ src/components/competitions/first_competition/Hero.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="flex flex-row">
+    <div class="flex flex-col w-1/2 ml-80 my-60 space-y-4">
+      <div class="text-3xl font-bold">
+        1st TradingLab Crypto Competition
+      </div>
+      <div class="text-7xl text-magentashine">
+        What will be BTC price at the end of May 2022?
+      </div>
+      <div class="text-4xl font-bold">
+        32 partecipants registered
+      </div>
+      <div>
+        <button
+          class="inline-block p-2 mt-2 text-3xl font-bold rounded hover:bg-blueshine bg-blueshine">
+          REAL TIME RANK
+        </button>
+      </div>
+    </div>
+    <div class="my-40 mr-64">
+      <img src="@/assets/competitions/first_competition/btc_micro.png" class="m-auto" alt="BtcMicro">
+    </div>
+  </div>
+</template>
---
+++ src/views/competitions/FirstCompetition.vue
@@ -0,0 +1,36 @@
+
+<template>
+  <div class="min-h-screen bg-universe text-cream">
+    <Header/>
+    <Hero/>
+    <Countdown class="mb-44"/>
+    <Footer/>
+  </div>
+</template>
+
+<script lang="ts">
+import {Component, Vue} from 'vue-property-decorator';
+import Header from '@/components/header/Header.vue';
+import Footer from '@/components/landing/Footer.vue';
+import Hero from '@/components/competitions/first_competition/Hero.vue'
+import Countdown from '@/components/competitions/first_competition/Countdown.vue'
+
+@Component({
+  components: {
+    Header,
+    Footer,
+    Hero,
+    Countdown,
+  },
+})
+export default class FirstCompetition extends Vue {
+  isMobile = false;
+  checkIfMobile() {
+    if ( screen.width <= 800 ) {
+      this.isMobile = true;
+    } else {
+      this.isMobile = false;
+    }
+  }
+}
+</script>
