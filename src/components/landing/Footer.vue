a83ea0ddba05e18e333978a17d33636b83be7919
dolphinxyz <robimalco@gmail.com>
2022-05-12 00:18:11 +0200

    #286 DONE

--- src/components/landing/Footer.vue
+++ src/components/landing/Footer.vue
@@ -1,36 +1,42 @@
 <template>
-  <div class="flex flex-col justify-center pb-20 -mt-20 text-center pt-44 bg-gradient-to-br from-universelight via-universe to-universe space-y-4">
-    <div class="flex flex-row justify-center xs:space-x-2 sm:space-x-6">
-      <button>
-        <a href="https://discord.gg/PEEUfrbQ9c" target="_blank">
-          <Discord class="transform xs:scale-50 sm:scale-75"/>
-        </a>
-      </button>
-      <button>
-        <a href="https://twitter.com/TradingLabXYZ" target="_blank">
-          <Twitter class="transform xs:scale-50 sm:scale-75"/>
-        </a>
-      </button>
-      <button>
-        <a href="https://github.com/TradingLabXYZ" target="_blank">
-          <Github class="transform xs:scale-50 sm:scale-75"/>
-        </a>
-      </button>
-      <button>
-        <a href="https://www.metafora.app/tradinglab" target="_blank">
-          <Metafora class="transform xs:scale-50 sm:scale-75"/>
-        </a>
-      </button>
-      <button>
-        <a href="https://www.youtube.com/channel/UC0MysGuteFUSLrTpzSHdoIg" target="_blank">
-          <Youtube class="transform xs:scale-50 sm:scale-75"/>
-        </a>
-      </button>
-      <button>
-        <a href="https://polygonscan.com/token/0x01962c00c783ce44c5b031c07435f2dc4a627394" target="_blank">
-          <Polyscan class="transform xs:scale-50 sm:scale-75"/>
-        </a>
-      </button>
+  <div class="flex flex-col justify-center pb-20 -mt-20 text-center pt-44 bg-gradient-to-br from-universelight
+  via-universe to-universe space-y-12">
+    <div class="font-bold text-cream space-y-3">
+      <div class="text-cream">
+        FOLLOW US
+      </div>
+      <div class="flex flex-row justify-center xs:space-x-2 sm:space-x-6">
+        <button>
+          <a href="https://discord.gg/PEEUfrbQ9c" target="_blank">
+            <Discord class="transform xs:scale-50 sm:scale-75"/>
+          </a>
+        </button>
+        <button>
+          <a href="https://twitter.com/TradingLabXYZ" target="_blank">
+            <Twitter class="transform xs:scale-50 sm:scale-75"/>
+          </a>
+        </button>
+        <button>
+          <a href="https://github.com/TradingLabXYZ" target="_blank">
+            <Github class="transform xs:scale-50 sm:scale-75"/>
+          </a>
+        </button>
+        <button>
+          <a href="https://www.metafora.app/tradinglab" target="_blank">
+            <Metafora class="transform xs:scale-50 sm:scale-75"/>
+          </a>
+        </button>
+        <button>
+          <a href="https://www.youtube.com/channel/UC0MysGuteFUSLrTpzSHdoIg" target="_blank">
+            <Youtube class="transform xs:scale-50 sm:scale-75"/>
+          </a>
+        </button>
+        <button>
+          <a href="https://polygonscan.com/token/0x01962c00c783ce44c5b031c07435f2dc4a627394" target="_blank">
+            <Polyscan class="transform xs:scale-50 sm:scale-75"/>
+          </a>
+        </button>
+      </div>
     </div>
     <div class="flex flex-row justify-center space-x-2">
       <img
---
+++ src/components/landing/Links.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="flex flex-col items-center -mt-20 align-middle bg-gradient-to-t from-universelight via-universe
+  to-universe text-cream py-44 sm:space-x-6 xs:space-y-6">
+    <h2 class="xs:text-2xl sm:text-4xl font-bold">
+      As seen on
+    </h2>
+    <div class="grid xs:grid-cols-1 sm:grid-cols-5 gap-x-1 gap-y-6">
+      <a 
+        href="https://www.dapp.com/app/tradinglab" 
+        target="_blank" 
+        class="m-auto">
+        <img
+          src="@/assets/partnersLogos/dappdotcom.png"
+          class="xs:w-24 sm:w-1/2"
+          alt="dapp.com logo">
+      </a>
+      <a 
+        href="https://dappradar.com/polygon/defi/tradinglab" 
+        target="_blank" 
+        class="m-auto">
+        <img
+          src="@/assets/partnersLogos/dappradar.png"
+          class="xs:w-24 sm:w-1/2"
+          alt="dappradar.com logo">
+      </a>
+      <a 
+        href="https://www.stateofthedapps.com/dapps/tradinglab" 
+        target="_blank" 
+        class="m-auto">
+        <img
+          src="@/assets/partnersLogos/stateofthedapps.png"
+          class="xs:w-24 sm:w-1/2"
+          alt="stateofthedapp.com logo">
+      </a>
+      <a 
+        href="https://dapp.expert/dapp/en-matic-tradinglab" 
+        target="_blank"
+        class="m-auto">
+        <img
+          src="@/assets/partnersLogos/dappexpert.png"
+          class="xs:w-24 sm:w-1/2"
+          alt="dapp.expert logo">
+      </a>
+      <a 
+        href="https://thehiveindex.com/communities/trading-lab" 
+        target="_blank"
+        class="m-auto">
+        <img
+          src="@/assets/partnersLogos/thehiveindex.png"
+          class="xs:w-24 sm:w-1/2"
+          alt="thehiveindex.com logo">
+      </a>
+    </div>
+  </div>
+</template>
+
+
+<script lang="ts">
+  import { Vue, Component } from 'vue-property-decorator';
+  @Component({})
+  export default class Links extends Vue {
+    selectedMode = 'learn';
+    selectMode(mode: string) {
+      this.selectedMode = mode;
+    }
+  }
+</script>
