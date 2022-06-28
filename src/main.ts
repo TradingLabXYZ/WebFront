7885f853b594f2e745ed88f3e9f8b6d208d6f2be
dolphinxyz <robimalco@gmail.com>
2022-06-28 11:53:23 +0200

    remove matomo

--- src/main.ts
+++ src/main.ts
@@ -3,17 +3,11 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import VueMeta from 'vue-meta'
-import VueMatomo from 'vue-matomo'
 import '@/assets/css/tailwind.css'
 Vue.config.productionTip = true;
 Vue.config.devtools = true;
 Vue.config.performance = true;
 Vue.use(VueMeta);
-Vue.use(VueMatomo, {
-  host: "https://analytics.tradinglab.xyz",
-  siteId: 1,
-  router: router
-});
 new Vue({
   store,
   router,
